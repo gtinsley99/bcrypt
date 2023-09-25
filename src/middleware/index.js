ee5f8c14e3db70169a4a90c44803cbf547dae2cd
George Tinsley <141220209+gtinsley99@users.noreply.github.com>
2023-09-25 14:22:26 +0100

    add bcrypt middleware to add user route working middleware/index.js

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,7 +1,11 @@
-const User = require("../models/users");
+const bcrypt = require("bcrypt");
+
 
 const hashPassword = async (req, res, next) => {
     try {
+        const saltRounds = parseInt(process.env.SALT)
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+        req.body.password = hashedPassword;
         console.log("Entering hashPassword try area");
         next();
     } catch (error) {
