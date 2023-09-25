9d95eda05f969684228bc82482c0bc7bfd75aa26
George Tinsley <141220209+gtinsley99@users.noreply.github.com>
2023-09-25 14:57:39 +0100

    add function to check if username + password matches db entry middleware/index.js

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,20 +1,47 @@
+const User = require("../models/users");
 const bcrypt = require("bcrypt");
 
-
+// Encrypts user password to store in db
 const hashPassword = async (req, res, next) => {
-    try {
-        const saltRounds = parseInt(process.env.SALT)
-        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
-        req.body.password = hashedPassword;
-        console.log("Entering hashPassword try area");
-        next();
-    } catch (error) {
-        console.log(error);
+  try {
+    const saltRounds = parseInt(process.env.SALT);
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+    req.body.password = hashedPassword;
+    console.log("Entering hashPassword try area");
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(501).json({
+      message: error.message,
+      error: error,
+    });
+  }
+};
+
+// Checks if username + password matches user in db
+const passwordCheck = async (req, res, next) => {
+  try {
+    const userDetails = await User.findOne({where: {username: req.body.username}});
+    const match = await bcrypt.compare(req.body.password, userDetails.password);
+    // If not a match gives error, doesn't reach next
+    if (!match) {
+        const error = new Error("Password and username do not match");
         res.status(501).json({
             message: error.message,
             error: error
         });
-    };
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(501).json({
+      message: error.message,
+      error: error,
+    });
+  }
 };
 
-module.exports = hashPassword;
+module.exports = {
+    hashPassword,
+    passwordCheck,
+};
