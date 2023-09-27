f8602678c3dda230f80a159a784d8d252dac96d2
George Tinsley <141220209+gtinsley99@users.noreply.github.com>
2023-09-27 11:04:35 +0100

    change comments routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,16 +6,16 @@ const { hashPassword, passwordCheck, tokenCheck } = require("../middleware");
 // Route to add a user, password hashed before add to db
 userRouter.post("/users/register", hashPassword, registerUser);
 
-// Route to list all users, passwordCheck 'guards' this
+// Route to list all users with token check
 userRouter.get("/users/listallusers", tokenCheck, listAllUsers);
 
-// Route to delete user, passwordCheck 'guards' this
+// Route to delete user with token check
 userRouter.delete("/users/deleteuser", tokenCheck, deleteUser);
 
-// Route to update email
+// Route to update email with token check
 userRouter.put("/users/updateemail", tokenCheck, updateEmail);
 
-// Route to update password
+// Route to update password with token check
 userRouter.put("/users/updatepassword", tokenCheck, hashPassword, updatePassword);
 
 // Route to login and create token
